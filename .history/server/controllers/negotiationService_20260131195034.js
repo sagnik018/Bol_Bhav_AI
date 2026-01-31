f946773ec73613a6a6221be58a94f303a0fc9221
Sagnik Saha <140305361+sagnik018@users.noreply.github.com>
2026-01-31 19:59:21 +0530

    Added all the new and modified files

---
+++ .history/server/controllers/negotiationService_20260131195034.js
@@ -0,0 +1,17 @@
+const { getGuidance } = require("../services/negotiationService");
+
+let lastResult = null;
+
+exports.negotiate = (req, res) => {
+  const price = parseInt(req.body.speech.match(/\d+/)[0]);
+  const market = { min: 80, max: 120 };
+
+  const action = getGuidance(price, market);
+  lastResult = { action };
+
+  res.json({ success: true });
+};
+
+exports.getResult = (req, res) => {
+  res.json(lastResult);
+};
